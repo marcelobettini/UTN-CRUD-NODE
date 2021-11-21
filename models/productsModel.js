507a2bcd3f97b146c662d0f71a1a31f82a3b3e30
unknown <marcelobettini@yahoo.com.ar>
2021-11-21 18:46:02 -0300

    crud operations fully working

--- models/productsModel.js
+++ models/productsModel.js
@@ -28,10 +28,28 @@ async function addProduct(data) {
   }
 }
 
-async function deleteItem(id) {
+/*función para modificar un registro existente en la base de datos, recibe un
+objeto con los nuevos datos y el id */
+async function modifyProduct(data, id) {
+  try {
+    const query = "UPDATE products SET ? WHERE id = ?";
+    const rows = await pool.query(query, [data, id]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+/*función para borrar un registro existente en la base de datos, recibe el id */
+async function deleteProduct(id) {
   const query = "DELETE FROM products WHERE id = ?";
   const rows = await pool.query(query, [id]);
   return rows;
 }
 
-module.exports = { getProducts, getProduct, addProduct, deleteItem };
+module.exports = {
+  getProducts,
+  getProduct,
+  addProduct,
+  deleteProduct,
+  modifyProduct,
+};
--- routes/listado.js
+++ routes/listado.js
@@ -10,10 +10,14 @@ router.get("/addItem", (req, res) => {
   res.render("addItem");
 });
 
+/* Ruta para agregar un producto nuevo, nótese que armamos el objeto con 
+la técnica de destructuring en las líneas 16 y 17. En la ruta para modificar lo haremos
+del modo tradicional para que pueda comparar*/
 router.post("/addItem", async (req, res) => {
-  const { name, origin, description, intensity, price, presentation } =
+  const { id, name, origin, description, intensity, price, presentation } =
     req.body;
   const data = {
+    id,
     name,
     origin,
     description,
@@ -26,6 +30,8 @@ router.post("/addItem", async (req, res) => {
   res.redirect("/listado");
 });
 
+/*Esta ruta muestra el producto para editarlo o borrarlo, así nos ahorramos
+la necesidad de hacer dos rutas y dos vistas distintas */
 router.get("/handleEdit/:id", async (req, res) => {
   const row = await productsModel.getProduct(req.params.id);
   const product = {
@@ -40,9 +46,29 @@ router.get("/handleEdit/:id", async (req, res) => {
   };
   res.render("editItem", { product });
 });
-router.get("/deleteItem/:id", async (req, res) => {
+
+/* Aquí modificamos un registro existente. construimos el objeto de la forma tradicional, sin 
+desestructurar. Es decir, los datos que se reciben por req.body se van asignando uno a uno
+a las propiedades correspondientes del objeto data */
+router.post("/editProduct", async (req, res) => {
+  const data = {
+    id: req.body.id,
+    name: req.body.name,
+    origin: req.body.origin,
+    description: req.body.origin,
+    intensity: req.body.intensity,
+    price: req.body.price,
+    presentation: req.body.presentation,
+    image: `https://picsum.photos/id/${req.body.image}/250`,
+  };
+  await productsModel.modifyProduct(data, data.id);
+  res.redirect("/listado");
+});
+
+/*Ruta para eliminar, recibe parámetro id*/
+router.get("/deleteProduct/:id", async (req, res) => {
   console.log(req.params.id);
-  await productsModel.deleteItem(req.params.id);
+  await productsModel.deleteProduct(req.params.id);
   res.redirect("/listado");
 });
 module.exports = router;
