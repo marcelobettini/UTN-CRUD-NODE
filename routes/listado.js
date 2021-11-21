ccf47c71f4b2b30a900e5149b31b7ca599a56d7d
unknown <marcelobettini@yahoo.com.ar>
2021-11-20 22:57:12 -0300

    addItem form ready

--- routes/listado.js
+++ routes/listado.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productsModel = require("../models/productsModel");
-
 router.get("/", async (req, res) => {
   const products = await productsModel.getProducts();
   res.render("listado", { user: req.session.user, products });
 });
 
-router.get("/agregar", (req, res) => {
+router.get("/addItem", (req, res) => {
   res.render("addItem");
 });
 module.exports = router;
